3fd85b18a967e71f4996cd9767e85eb2f4734c80
saichittala <110885282+saichittala@users.noreply.github.com>
2026-05-01 02:25:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -49,3 +49,13 @@ const observer = new IntersectionObserver(entries => {
 });
 
 reveals.forEach(el => observer.observe(el));
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
